fc673429fa6f8f7e63cd6959655b4d4e1194a711
Angelo <herman256561@gmail.com>
2024-08-21 11:18:40 -0400

    sign in function completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css';
 import { Children } from 'react';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 
 export const metadata = {
     title: "Promptopia",
@@ -10,12 +13,15 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient' />
             </div>
             <main className='app'>
+                <Nav />
                 {children}
             </main>
+            </Provider>
         </body>
     </html>
   )
