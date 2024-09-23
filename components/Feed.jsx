0c75bc49911c2cd15598749c95edb7c1b9e77f57
Angelo <herman256561@gmail.com>
2024-09-23 16:08:23 -0400

    api test4

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -25,9 +25,9 @@ const Feed = () => {
   const handleSearchChange = (e) =>{
 
   }
-  
+  const idd = "66c604972020b851d45fcb71";
   const fetchPosts = async () => {
-    const response = await fetch(`/api/prompt`);
+    const response = await fetch(`/api/users/${idd}/posts`);
     if (!response.ok) {
       throw new Error('Failed to fetch: ' + response.statusText);
     }
