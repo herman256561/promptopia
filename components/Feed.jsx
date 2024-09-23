460245295932382c9f3a473e5c7a5d344d9eebe8
Angelo <herman256561@gmail.com>
2024-09-23 14:29:06 -0400

    add router.asPath

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import PromptCard from './PromptCard';
 
 const PromptCardList = ({data, handleTagClick}) => {
@@ -21,19 +22,22 @@ const PromptCardList = ({data, handleTagClick}) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
   const [posts, setPosts] = useState([]);
+  const router = useRouter;
   const handleSearchChange = (e) =>{
 
   }
 
   useEffect(()=>{
+    
     const fetchPosts = async() => {
       const response = await fetch('/api/prompt');
       const data = await response.json();
+      
       setPosts(data);
     }
     
     fetchPosts();
-  }, []);
+  }, [router.asPath]);
 
   return (
     <section className='feed'>
